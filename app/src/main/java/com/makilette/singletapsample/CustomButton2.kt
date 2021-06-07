0f5097af77f383a302be251f145f15b51a5d050d
Reo Ando <ando.reo420@gmail.com>
2021-06-08 00:15:19 +0900

    OnTouchイベントを抑止するKotlinの拡張関数を作成する

--- app/src/main/java/com/makilette/singletapsample/CustomButton2.kt
+++ app/src/main/java/com/makilette/singletapsample/CustomButton2.kt
@@ -4,32 +4,24 @@ import android.content.Context
 import android.os.SystemClock
 import android.util.AttributeSet
 import android.util.Log
-import android.widget.Button
-import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.button.MaterialButton
 
 /**
- * 連続タップ制御: performClick
+ * SingleClick: performClick
  *
- * 2019-07-01 21:02:40.679 25251-25251/com.makilette.singletapsample D/--ando--: Enable click.
- * 2019-07-01 21:02:40.683 25251-25251/com.makilette.singletapsample D/MainActivity: --ando-- onClick id: 2131165219
- * 2019-07-01 21:02:40.683 25251-25251/com.makilette.singletapsample D/MainActivity: --ando-- タップ制御: performClick
- * 2019-07-01 21:02:42.426 25251-25251/com.makilette.singletapsample D/MainActivity: --ando-- startActivity
- * 2019-07-01 21:02:42.471 25251-25251/com.makilette.singletapsample D/--ando--: Disable click for a certain period.
- * 2019-07-01 21:02:42.471 25251-25251/com.makilette.singletapsample D/--ando--: Disable click for a certain period.
- * 2019-07-01 21:02:42.525 25251-25251/com.makilette.singletapsample D/--ando--: Disable click for a certain period.
- * 2019-07-01 21:02:42.526 25251-25251/com.makilette.singletapsample D/--ando--: Disable click for a certain period.
+ * 2021-06-08 00:12:21.248 13638-13638/leo.singletapsample D/CustomButton2: Enable click.
+ * 2021-06-08 00:12:21.248 13638-13638/leo.singletapsample D/MainActivity: CustomButton2 performClick activity
+ * 2021-06-08 00:12:22.650 13638-13638/leo.singletapsample D/CustomButton2: Disable click for a certain period.
+ * 2021-06-08 00:12:23.318 13638-13638/leo.singletapsample D/CustomButton2: Enable click.
+ * 2021-06-08 00:12:23.318 13638-13638/leo.singletapsample D/MainActivity: CustomButton2 performClick activity
  *
- * 検証端末:
- * Google Pixcel3
- * OS: 9
  */
 class CustomButton2(context: Context, attrs: AttributeSet) : MaterialButton(context, attrs) {
 
-    private var mLastClickTime: Long = 0
+    private var lastClickTime: Long = 0
 
     /** クリック無効期間. */
-    private var mClickDisablePeriod: Long = 2000
+    private var clickDisablePeriod: Long = 2000
 
     /**
      * [android.view.View.performClick]
@@ -40,15 +32,15 @@ class CustomButton2(context: Context, attrs: AttributeSet) : MaterialButton(cont
      */
     override fun performClick(): Boolean {
         // 一定期間、クリックを無効化する.
-        if (SystemClock.elapsedRealtime() - mLastClickTime < mClickDisablePeriod) {
-            Log.d("--ando--", "Disable click for a certain period.")
+        if (SystemClock.elapsedRealtime() - lastClickTime < clickDisablePeriod) {
+            Log.d("CustomButton2", "Disable click for a certain period.")
             // OnClickListenerを呼び出さない.
             return false
         }
-        mLastClickTime = SystemClock.elapsedRealtime()
+        lastClickTime = SystemClock.elapsedRealtime()
 
         // 通常のクリックイベント処理を行う.OnClickListenerを呼び出す.
-        Log.d("--ando--", "Enable click.")
+        Log.d("CustomButton2", "Enable click.")
         return super.performClick()
     }
 
@@ -56,6 +48,6 @@ class CustomButton2(context: Context, attrs: AttributeSet) : MaterialButton(cont
      *  クリック無効期間を変更可能する.
      */
     fun setClickDisablePeriod(clickDisablePeriod: Long) {
-        this.mClickDisablePeriod = clickDisablePeriod
+        this.clickDisablePeriod = clickDisablePeriod
     }
 }
--- app/src/main/java/com/makilette/singletapsample/MainActivity.kt
+++ app/src/main/java/com/makilette/singletapsample/MainActivity.kt
@@ -3,12 +3,14 @@ package com.makilette.singletapsample
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.makilette.singletapsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val TAG = MainActivity::class.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,30 +20,33 @@ class MainActivity : AppCompatActivity() {
         binding.activity = this
 
         binding.button1.setOnClickListener {
-            Log.d("MainActivity", "--ando-- タップ制御: onTouchEvent")
+            Log.d(TAG, binding.button1.text.toString())
             startActivity2()
         }
         binding.button2.setOnClickListener {
-            Log.d("MainActivity", "--ando-- タップ制御: performClick")
+            Log.d(TAG, binding.button2.text.toString())
             startActivity2()
         }
         binding.button5.setOnClickListener {
-            Log.d("MainActivity", "--ando-- タップ制御なし")
+            Log.d(TAG, binding.button5.text.toString())
             startActivity2()
         }
-        binding.button7.setOnSingleClickListener(View.OnClickListener {
-            Log.d("MainActivity", "--ando-- タップ制御: カスタムクリックリスナー")
+        binding.button7.setOnSingleClickListener {
+            Log.d(TAG, binding.button7.text.toString())
             startActivity2()
-        })
+        }
         binding.button8.setOnDebounceClickListener {
-            Log.d("MainActivity", "--ando-- タップ制御: setOnDebounceClickListener")
+            Log.d(TAG, binding.button8.text.toString())
             startActivity2()
+        }
+        binding.button9.setOnSingleTouchListener {
+            Log.d(TAG, binding.button9.text.toString())
         }
     }
 
     fun startActivity2() {
         // TODO ここにブレークポイント貼ってデバックで連打試験する.
-        Log.d("MainActivity", "--ando-- startActivity")
+        Log.d(TAG, "startActivity2")
         val intent = Intent(this, SubActivity::class.java)
         startActivity(intent)
     }
